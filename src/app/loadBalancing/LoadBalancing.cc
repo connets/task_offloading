0620453db7d6294ff347311000412bf5b0131a7e
LucaParenti <luca.parenti1@studenti.unimi.it>
2023-01-11 14:36:43 +0100

    State pattern initial implementation

---
+++ src/app/loadBalancing/LoadBalancing.cc
@@ -0,0 +1,46 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "LoadBalancing.h"
+
+using namespace tirocinio;
+
+LoadBalancingContext::LoadBalancingContext(LoadBalancingState* newState)
+{
+    currentState = newState;
+}
+
+void LoadBalancingContext::setState(LoadBalancingState* newState)
+{
+    if (currentState) {
+        delete currentState;
+        currentState = newState;
+    }
+}
+
+bool LoadBalancingContext::getCurrentState()
+{
+    return currentState->getCurrentState();
+}
+
+bool Active::getCurrentState()
+{
+    return false;
+}
+
+bool Disabled::getCurrentState()
+{
+    return true;
+}
