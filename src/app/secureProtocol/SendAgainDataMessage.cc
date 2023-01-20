246242a0c7573ca911fd2996cc4154e71017fe3b
LucaParenti <luca.parenti.work@gmail.com>
2023-01-20 18:14:18 +0100

    Implemented function that send again the data if the bus doesn't receive the response in computation time

---
+++ src/app/secureProtocol/SendAgainDataMessage.cc
@@ -0,0 +1,40 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "app/VeinsApp.h"
+#include "app/messages/ComputationTimerMessage_m.h"
+
+using namespace tirocinio;
+
+void VeinsApp::sendAgainData(int index, double load)
+{
+    auto found = helpersLoad.find(index);
+    if (found != helpersLoad.end()) {
+        // Prepare the new data message
+        DataMessage* dataMsg = new DataMessage();
+        populateWSM(dataMsg);
+        dataMsg->setHostIndex(index);
+        dataMsg->setLoadToProcess(load);
+        sendDown(dataMsg);
+
+        // Restart again the timer
+        ComputationTimerMessage* computationTimerMsg = new ComputationTimerMessage();
+        populateWSM(computationTimerMsg);
+        computationTimerMsg->setSimulationTime(simTime());
+        computationTimerMsg->setIndexHost(index);
+        computationTimerMsg->setLoadHost(load);
+        scheduleAt(simTime() + 10 + uniform(5, 10), computationTimerMsg);
+    }
+}
