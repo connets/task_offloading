d086fd3cfc81bd4104247b31058cd46ff982c176
LucaParenti <luca.parenti1@studenti.unimi.it>
2023-03-30 08:50:51 +0200

    Added to HelperVehicleInfo class its documentation

--- src/app/vehiclesHandling/HelperVehicleInfo.cc
+++ src/app/vehiclesHandling/HelperVehicleInfo.cc
@@ -35,38 +35,77 @@ HelperVehicleInfo::~HelperVehicleInfo() {
     //
 }
 
+/**
+ * @returns Current load of a vehicle in MB
+ */
 double HelperVehicleInfo::getCurrentLoad() {
     return this->hostCurrentLoad;
 }
 
+/**
+ * @returns Current CPU frequency of a vehicle in GHz
+ */
 double HelperVehicleInfo::getCPUFreq() {
     return this->hostCPUFreq;
 }
 
+/**
+ * @returns Timestamp corresponding to when the object is created in s
+ */
 simtime_t HelperVehicleInfo::getCreatedAt() {
     return this->createdAt;
 }
 
+/**
+ * @returns The L2 address of a vehicle
+ */
 veins::LAddress::L2Type HelperVehicleInfo::getAddress() {
     return this->address;
 }
 
+/**
+ * Set the current load of a vehicle
+ *
+ * @param newLoad The new load of a vehicle
+ */
 void HelperVehicleInfo::setCurrentLoad(double newLoad) {
     this->hostCurrentLoad = newLoad;
 }
 
+/**
+ * Set the current CPU frequency of a vehicle
+ *
+ * @param newFreq The new frequency of a vehicle
+ */
 void HelperVehicleInfo::setCPUFreq(double newFreq) {
     this->hostCPUFreq = newFreq;
 }
 
+/**
+ * Set the creation time of the object
+ *
+ * @param newTime Timestamp when object is created
+ */
 void HelperVehicleInfo::setCreatedAt(simtime_t newTime) {
     this->createdAt = newTime;
 }
 
+/**
+ * Set the current L2 address of a vehicle
+ *
+ * @param newAddress The new address of a vehicle
+ */
 void HelperVehicleInfo::setAddress(veins::LAddress::L2Type newAddress) {
     this->address = newAddress;
 }
 
+/**
+ * This method calculate the toal computation time of a task
+ * considering the current load and the CPU frequency of a vehicle
+ *
+ * @param CPI Clock per instructions
+ * @returns The total computation time for a task
+ */
 double HelperVehicleInfo::getTotalComputationTime(int CPI) {
     return (CPI * this->hostCurrentLoad * (1 / this->hostCPUFreq));
 }
