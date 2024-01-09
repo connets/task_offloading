971a4742fb6f9663ff508112a8ce01f0ef8b0a5b
LucaParenti <luca.parenti1@studenti.unimi.it>
2024-01-09 15:15:04 +0100

    Modified Dockerfile, Makefile and launcher.sh to include references to veins_inet project since now the task_offloading project is using that framework

--- launcher.sh
+++ launcher.sh
@@ -9,6 +9,6 @@ RES_DIR=/app/results
 
 # Launch simulations
 opp_runall -j8 -b1 task_offloading/out/gcc-release/src/task_offloading -m -u Cmdenv --cmdenv-stop-batch-on-error=false \
-	-n task_offloading/simulations:task_offloading/src:inet/src:veins-veins-5.2/src/veins \
-	-l inet/src/INET -l veins-veins-5.2/src/veins \
+	-n task_offloading/simulations:task_offloading/src:inet/src:veins-veins-5.2/src/veins:veins-veins-5.2/subprojects/veins_inet/src/veins_inet \
+	-l inet/src/INET -l veins-veins-5.2/src/veins -l veins-veins-5.2/subprojects/veins_inet/src/veins_inet \
 	task_offloading/simulations/task_offloading/omnetpp.ini -c Default --result-dir=$RES_DIR
